7d7d7c7043af0ed32041de178c648ca65748b210
Nandhelo <131649728+Nandhelo@users.noreply.github.com>
2023-05-02 16:35:11 +0800

    Add files via upload
    week1.day2

---
+++ day2/convertNegativetoPositive.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class convertNegativetoPositive {
+public static void main(String[] args) {
+		
+		//Convert the given number from negative to positive
+		int num = -40;
+		int positive;
+		if(num<0) {
+			positive = num * -1;
+			System.out.println("The number "+num+" is converted into "+positive);
+		}
+	}
+
+
+}
